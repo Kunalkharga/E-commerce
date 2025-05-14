94446dd8ac96493dc70aa7d2a8e6624d10c3d8a1
kunal kharga <kunalkg93@hmail.com>
2025-05-14 18:10:04 +0545

    Code modified

--- E-commerce.js
+++ E-commerce.js
@@ -270,13 +270,3 @@ document.querySelectorAll('.faq-question').forEach(question => {
     }
   });
 });
-
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
