bd7140be2cf5a35765f4adcd488ead9373aaad47
kunal kharga <kunalkg93@gmail.com>
2025-06-30 21:41:47 +0545

    Cart updated

--- includes/config.php
+++ includes/config.php
@@ -1,18 +1,18 @@
 <?php
-// define('DB_HOST', getenv('DB_HOST'));
-// define('DB_USER', getenv('DB_USER'));
-// define('DB_PASS', getenv('DB_PASS'));
-// define('DB_NAME', getenv('DB_NAME'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_NAME', getenv('DB_NAME'));
 
-// define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost/');
+define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost/');
 
 
 // <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root'); // Replace with your MySQL username
-define('DB_PASS', ''); // Replace with your MySQL password
-define('DB_NAME', 'doggy_ecommerce');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root'); // Replace with your MySQL username
+// define('DB_PASS', ''); // Replace with your MySQL password
+// define('DB_NAME', 'doggy_ecommerce');
 
-define('BASE_URL', 'http://localhost/doggy-ecommerce/');
-// ?>
+// define('BASE_URL', 'http://localhost/doggy-ecommerce/');
+?>
