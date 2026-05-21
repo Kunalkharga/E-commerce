b0bcdab94b32128a76702ff13b79cbe10a7248a4
kunal kharga <kunalkg93@gmail.com>
2026-05-21 22:54:39 +0545

    Push all remaining project files

--- includes/config.php
+++ includes/config.php
@@ -6,4 +6,12 @@ define('DB_NAME', getenv('DB_NAME'));
 
 define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost/');
 
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root'); // Replace with your MySQL username
+// define('DB_PASS', ''); // Replace with your MySQL password
+// define('DB_NAME', 'doggy_ecommerce');
+
+// define('BASE_URL', 'http://localhost/doggy-ecommerce/');
+
 ?>
